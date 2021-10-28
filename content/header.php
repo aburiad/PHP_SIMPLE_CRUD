b41b8fe959b79b8a3dc2d5c3e84d39fe85810ba0
Abu Riad <43032272+aburiad@users.noreply.github.com>
2021-10-28 17:32:22 +0600

    Add files via upload
    folder upload

---
+++ content/header.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+    <meta charset="utf-8">
+    <title>student data Record</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Place favicon.ico in the root directory -->
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <!-- css stylesheet here  -->
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <style>
+    </style>
+</head>
+
+<body>
+
+
+    <!-- start data saved output area  -->
+    <div class="container">
+        <div class="heading_area">
+            <h2>Dynamic Student Data Record System</h2>
+            <p>find out your data easily</p>
+        </div>
+        <div class="nessasary_text">
+            <p class="same_style">student list</p>
+            <a class="same_style btn-right" href="insert.php">add new data</a>
+        </div>
+    </div>
+    <!-- end data saved output area  -->
